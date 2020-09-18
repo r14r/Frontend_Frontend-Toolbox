72a84dbfda7cfd6cf32d786b1ca068c6c3c6b274
Ralph Goestenmeier <r14r@via-internet.de>
2020-09-18 10:04:59 +0200

    master | -

---
+++ samples/animation_on_scroll/split_text_image_scroll/default.js
@@ -0,0 +1,12 @@
+$(document).ready(function ($) {
+    console.clear();
+
+    ScrollOut({
+        cssProps: {
+            visibleY: true,
+            viewportY: true,
+            scrollPercentY: true,
+        },
+        threshold: 0.2,
+    });
+});
